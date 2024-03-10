0b6c825e2a1d7cebb5df3600164b49379e79c66c
Art <art-75@yandex.ru>
2024-03-10 10:46:08 +0300

    Initial commit

---
+++ job_market/job_market/urls.py
@@ -0,0 +1,20 @@
+"""
+Some Infos
+"""
+from django.contrib import admin
+from django.urls import path, include
+from myapp import views as views_myapp
+from django.conf import settings  # настройка media
+from django.conf.urls.static import static  # настройка media
+import debug_toolbar
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("", views_myapp.hello_world),  # Таким образом вывел для себя
+    path("myapp/", include('myapp.urls')),
+    # path('__debug__/', include("debug_toolbar.urls")), - закомментировав это, отключаем режим отладки
+]
+
+# настройка media
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
